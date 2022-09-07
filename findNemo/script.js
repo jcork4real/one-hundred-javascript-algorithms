83a87aa309c166122de2625fa1b89922cef469ad
Jarone McCorkle <jaroneemccorkle@gmail.com>
2022-09-07 17:48:54 -0600

    Updated findNemo

--- findNemo/script.js
+++ findNemo/script.js
@@ -2,6 +2,21 @@
 
 const nemo = ["nemo"];
 
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+
+const large = new Array(1000).fill("nemo");
+
 function findNemo(array) {
   let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
@@ -13,4 +28,4 @@ function findNemo(array) {
   document.write(`<br><br>Call to find Nemo took ${t1 - t0} milliseconds`);
 }
 
-findNemo(nemo);
+findNemo(large);

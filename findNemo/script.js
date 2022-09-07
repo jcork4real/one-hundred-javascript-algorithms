e5d375b372bf90c6ddc5ef8337d9e62a34dc2f24
Jarone McCorkle <jaroneemccorkle@gmail.com>
2022-09-07 17:35:20 -0600

    Updated JS

--- findNemo/script.js
+++ findNemo/script.js
@@ -3,11 +3,14 @@
 const nemo = ["nemo"];
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       document.write("Found Nemo!");
     }
   }
+  let t1 = performance.now();
+  document.write(`<br><br>Call to find Nemo took ${t1 - t0} milliseconds`);
 }
 
 findNemo(nemo);
